557310cd34c00eab84d2c7878463509e244b0ed2
sauzzon <saujan.tiwari@gmail.com>
2022-11-06 22:33:21 +0545

    Communicating between containers

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+  .connect("mongodb://saujan:mypassword@mongo:27017/?authSource=admin")
+  .then(() => console.log("Succesfully connected to DB"))
+  .catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
   res.send("<h2>Hello! from docker container</h2>");
